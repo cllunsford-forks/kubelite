6629314ecd49bdfedfae5cdbf5072be9b3888733
Chris Lunsford <cllunsford@gmail.com>
2016-05-26 20:16:02 -0400

    Adding v1beta1 extensions, updating all to HEAD

--- gen.sh
+++ gen.sh
@@ -6,18 +6,25 @@ branch="master"
 unversioned="$prefix/$branch/pkg/api/unversioned/types.go"
 v1="$prefix/$branch/pkg/api/v1/types.go"
 res="$prefix/$branch/pkg/api/resource/quantity.go"
+v1beta1="$prefix/$branch/pkg/apis/extensions/v1beta1/types.go"
 
 echo "Fetching unversioned/types.go"
 curl -o unversioned/types.go -SsL $unversioned
 echo "Fetching v1/types.go"
 curl -o v1/types.go -SsL $v1
 #curl -o resource/quantity.go -SsL $res
+echo "Fetching extensions/v1beta1/types.go"
+curl -o extensions/v1beta1/types.go -SsL $v1beta1
 
-echo "Rewriting imports on v1/types.go"
-perl -pi -e "s|k8s\.io/kubernetes/pkg/api/unversioned|github.com/technosophos/kubelite/unversioned|g"  v1/types.go
-perl -pi -e "s|k8s\.io/kubernetes/pkg/api/resource|github.com/technosophos/kubelite/resource|g"  v1/types.go
-perl -pi -e "s|k8s\.io/kubernetes/pkg/types|github.com/technosophos/kubelite/types|g"  v1/types.go
-perl -pi -e "s|\"k8s\.io/kubernetes/pkg/util\"||g" v1/types.go
-perl -pi -e "s|\"k8s\.io/kubernetes/pkg/runtime\"||g" v1/types.go
-perl -pi -e "s|util\.IntOrString|types.IntOrString|g" v1/types.go
-perl -pi -e "s|runtime\.RawExtension|types.RawExtension|g" v1/types.go
+
+for target in "v1/types.go" "extensions/v1beta1/types.go"; do
+  echo "Rewriting imports on $target"
+  perl -pi -e "s|k8s\.io/kubernetes/pkg/api/unversioned|github.com/technosophos/kubelite/unversioned|g" $target
+  perl -pi -e "s|k8s\.io/kubernetes/pkg/api/resource|github.com/technosophos/kubelite/resource|g" $target
+  perl -pi -e "s|k8s\.io/kubernetes/pkg/api/v1|github.com/technosophos/kubelite/v1|g" $target
+  perl -pi -e "s|k8s\.io/kubernetes/pkg/types|github.com/technosophos/kubelite/types|g" $target
+  perl -pi -e "s|\"k8s\.io/kubernetes/pkg/util\"||g" $target
+  perl -pi -e "s|\"k8s\.io/kubernetes/pkg/runtime\"||g" $target
+  perl -pi -e "s|util\.IntOrString|types.IntOrString|g" $target
+  perl -pi -e "s|runtime\.RawExtension|types.RawExtension|g" $target
+done
